afe116ba121418c2578786d271f316c8270bb110
gustavocadev <gustavocadev@gmail.com>
2024-11-09 18:09:00 -0500

    Add product filtering by name in load function

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,13 +1,17 @@
 import type { ProductsResponse } from '../../pocketbase-types.js';
 import type { ProductPricesResponse } from '../../pocketbase-types';
 
-export const load = async ({ locals }) => {
+export const load = async ({ locals, request }) => {
+  const url = new URL(request.url);
+  const productName = url.searchParams.get('p');
+
   const products = locals.pb.collection('products').getFullList<
     ProductsResponse<{
       product_prices_via_product_id?: ProductPricesResponse[];
     }>
   >({
     expand: 'product_prices_via_product_id',
+    filter: productName ? `name ~ "${productName}"` : '',
   });
 
   return {
