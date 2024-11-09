932f643ff464cdeed61cf1b6034f41b5ec15e082
gustavocadev <gustavocadev@gmail.com>
2024-11-09 14:43:07 -0500

    Add CartSheet component to layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+    import CartSheet from '$lib/components/cart-sheet.svelte';
   import Navbar from '$lib/components/shared/nav.svelte'
   import { setCartContextProvider } from '$lib/context/cart.svelte';
   import '../app.css'
@@ -12,3 +13,5 @@
 <main class="container mx-auto px-4 sm:px-6 lg:px-8 py-8">
   {@render children()}
 </main>
+
+<CartSheet />
