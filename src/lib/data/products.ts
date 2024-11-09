84666d39e4aed0dd3bf487d682aed582ee976134
gustavocadev <gustavocadev@gmail.com>
2024-11-09 11:52:18 -0500

    Add product data model and sample products

---
+++ src/lib/data/products.ts
@@ -0,0 +1,66 @@
+type Product = {
+  id: number;
+  name: string;
+  price: string;
+  imageSrc: string;
+  imageAlt: string;
+};
+
+export const products: Product[] = [
+  {
+    id: 1,
+    name: 'Peluche de Oso Polar',
+    price: '$25',
+    imageSrc: 'https://placehold.co/500x500',
+    imageAlt: 'Peluche de Oso Polar',
+  },
+  {
+    id: 2,
+    name: 'Peluche de Tigre',
+    price: '$30',
+    imageSrc: 'https://placehold.co/500x500',
+    imageAlt: 'Peluche de Tigre',
+  },
+  {
+    id: 3,
+    name: 'Peluche de Panda',
+    price: '$35',
+    imageSrc: 'https://placehold.co/500x500',
+    imageAlt: 'Peluche de Panda',
+  },
+  {
+    id: 4,
+    name: 'Peluche de Elefante',
+    price: '$40',
+    imageSrc: 'https://placehold.co/500x500',
+    imageAlt: 'Peluche de Elefante',
+  },
+  {
+    id: 5,
+    name: 'Peluche de León',
+    price: '$30',
+    imageSrc: 'https://placehold.co/500x500',
+    imageAlt: 'Peluche de León',
+  },
+  {
+    id: 6,
+    name: 'Peluche de Jirafa',
+    price: '$28',
+    imageSrc: 'https://placehold.co/500x500',
+    imageAlt: 'Peluche de Jirafa',
+  },
+  {
+    id: 7,
+    name: 'Peluche de Koala',
+    price: '$27',
+    imageSrc: 'https://placehold.co/500x500',
+    imageAlt: 'Peluche de Koala',
+  },
+  {
+    id: 8,
+    name: 'Peluche de Delfín',
+    price: '$26',
+    imageSrc: 'https://placehold.co/500x500',
+    imageAlt: 'Peluche de Delfín',
+  },
+];
