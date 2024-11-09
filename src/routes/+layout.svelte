c246ab2f0747dd4597da93300e5628f4aaf493f9
gustavocadev <gustavocadev@gmail.com>
2024-11-09 13:41:30 -0500

    Initialize cart context provider in layout component

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,14 +1,14 @@
 <script lang="ts">
   import Navbar from '$lib/components/shared/nav.svelte'
+  import { setCartContextProvider } from '$lib/context/cart.svelte';
   import '../app.css'
+
 	let { children } = $props();
+
+  setCartContextProvider()
 </script>
 
-
-<Navbar
-  quantity={0}
-  isOpen={false}
-/>
+<Navbar />
 <main class="container mx-auto px-4 sm:px-6 lg:px-8 py-8">
   {@render children()}
 </main>
