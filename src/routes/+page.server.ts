69fef118db0ed0f4f9c735232cea70eecb040317
gustavocadev <gustavocadev@gmail.com>
2024-11-09 16:48:16 -0500

    Add product loading logic and skeleton UI for products display

---
+++ src/routes/+page.server.ts
@@ -0,0 +1,16 @@
+import type { ProductsResponse } from '../../pocketbase-types.js';
+import type { ProductPricesResponse } from '../../pocketbase-types';
+
+export const load = async ({ locals }) => {
+  const products = locals.pb.collection('products').getFullList<
+    ProductsResponse<{
+      product_prices_via_product_id: ProductPricesResponse[];
+    }>
+  >({
+    expand: 'product_prices_via_product_id',
+  });
+
+  return {
+    products,
+  };
+};
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,25 +1,31 @@
 <script lang="ts">
-  import { products } from '$lib/data/products';
+  import { Skeleton } from '$lib/components/ui/skeleton';
+
+  let { data } = $props();
+
+  const skeletonArr = Array.from({ length: 8 });
 </script>
 
-<main class="container mx-auto px-4 sm:px-6 lg:px-8 py-8">
-  <h2 class="text-3xl font-bold text-gray-900">Home</h2>
-  <p class="mt-1 text-sm text-gray-500">Todos los productos</p>
+<h2 class="text-3xl font-bold text-gray-900">Home</h2>
+<p class="mt-1 text-sm text-gray-500">Todos los productos</p>
 
-  <div
-    class="mt-6 grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8"
-  >
-    {#each products as product}
+<div
+  class="mt-6 grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8"
+>
+  {#await data.products}
+    {#each skeletonArr as skeleton}
+      <Skeleton class="w-full h-[320px]" />
+    {/each}
+  {:then products}
+    {#each products as product, idx}
       <div class="group relative">
         <figure
           class="w-full min-h-80 aspect-w-1 aspect-h-1 rounded-md overflow-hidden group-hover:opacity-75 lg:h-80 lg:aspect-none"
         >
           <img
-            src={product.imageSrc}
-            alt={product.imageAlt}
+            src={`https://placedog.net/50${idx}`}
+            alt={product.description}
             class="w-full h-full object-center object-cover lg:w-full lg:h-full"
-            width={500}
-            height={500}
           />
         </figure>
         <div class="mt-4 flex justify-between">
@@ -31,9 +37,11 @@
               </a>
             </h3>
           </div>
-          <p class="text-sm font-medium text-gray-900">{product.price}</p>
+          <p class="text-sm font-medium text-gray-900">
+            S/. {product.expand?.product_prices_via_product_id[0].price}
+          </p>
         </div>
       </div>
     {/each}
-  </div>
-</main>
+  {/await}
+</div>
