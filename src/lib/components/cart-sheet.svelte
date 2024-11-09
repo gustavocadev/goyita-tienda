1ef387ae7945ffc7fe96664e9fbbf62b974bec14
gustavocadev <gustavocadev@gmail.com>
2024-11-09 14:48:34 -0500

    Add cartProducts state

--- src/lib/components/cart-sheet.svelte
+++ src/lib/components/cart-sheet.svelte
@@ -11,6 +11,21 @@
     setOpenCartSheet,
     isOpenCartSheet,
   } = getCartContext();
+
+  let cartProducts = $state([
+    {
+      id: 1,
+      title: 'Título del Producto',
+      price: 15.0,
+      image: 'https://placehold.co/64x64',
+    },
+    {
+      id: 2,
+      title: 'Título del Producto',
+      price: 15.0,
+      image: 'https://placehold.co/64x64',
+    },
+  ]);
 </script>
 
 <Sheet
@@ -19,9 +34,7 @@
     setOpenCartSheet(value);
   }}
 >
-  <SheetContent
-    class="w-[360px] sm:max-w-[360px] p-0 max-h-screen overflow-y-auto"
-  >
+  <SheetContent class="w-[360px] sm:max-w-[360px] p-0 h-full">
     <SheetHeader class="p-4 pb-0">
       <div class="flex items-center justify-between">
         <SheetTitle class="text-xl">Mi Carrito</SheetTitle>
@@ -29,47 +42,49 @@
     </SheetHeader>
     <div class="flex flex-col h-full">
       <div class="flex-1 overflow-auto p-4">
-        <div class="flex gap-3 pb-4 border-b border-white/10">
-          <div class="relative h-16 w-16 overflow-hidden rounded bg-white/5">
-            <img
-              src="https://placehold.co/64x64"
-              alt="Imagen del producto"
-              width={64}
-              height={64}
-              class="object-cover"
-            />
-          </div>
-          <div class="flex flex-1 flex-col justify-between">
-            <div>
-              <h3 class="font-medium">Título del Producto</h3>
-              <p class="text-sm text-white/60">Negro / XS</p>
-            </div>
-            <div class="flex items-center justify-between">
-              <span class="font-medium">$15.00 USD</span>
-              <div class="flex items-center gap-3 bg-white/5 rounded-lg">
-                <Button
-                  variant="ghost"
-                  size="icon"
-                  class="h-8 w-8"
-                  onclick={() => decrementQuantity()}
-                >
-                  <Minus class="h-3 w-3" />
-                </Button>
-                <span class="w-4 text-center">{quantityCartSheet()}</span>
-                <Button
-                  variant="ghost"
-                  size="icon"
-                  class="h-8 w-8"
-                  onclick={() => incrementQuantity()}
-                >
-                  <Plus class="h-3 w-3" />
-                </Button>
+        {#each cartProducts as cartProduct}
+          <div class="flex gap-3 pb-4 border-b">
+            <figure class="relative h-16 w-16 overflow-hidden rounded">
+              <img
+                src="https://placehold.co/64x64"
+                alt="Imagen del producto"
+                width={64}
+                height={64}
+                class="object-cover"
+              />
+            </figure>
+            <div class="flex flex-1 flex-col justify-between">
+              <div>
+                <h3 class="font-medium">Título del Producto</h3>
+                <p class="text-sm">Negro / XS</p>
+              </div>
+              <div class="flex items-center justify-between">
+                <span class="font-medium">$15.00 USD</span>
+                <div class="flex items-center gap-3 bg-white/5 rounded-lg">
+                  <Button
+                    variant="ghost"
+                    size="icon"
+                    class="h-8 w-8"
+                    onclick={() => decrementQuantity()}
+                  >
+                    <Minus class="h-3 w-3" />
+                  </Button>
+                  <span class="w-4 text-center">{quantityCartSheet()}</span>
+                  <Button
+                    variant="ghost"
+                    size="icon"
+                    class="h-8 w-8"
+                    onclick={() => incrementQuantity()}
+                  >
+                    <Plus class="h-3 w-3" />
+                  </Button>
+                </div>
               </div>
             </div>
           </div>
-        </div>
+        {/each}
       </div>
-      <div class="border-t border-white/10 p-4">
+      <div class="border-t p-4 mb-12">
         <div class="space-y-3">
           <div class="flex justify-between text-sm">
             <span>Impuestos</span>
