1cf04d79b051deb6236bf2f0b9697a8a511cd246
gustavocadev <gustavocadev@gmail.com>
2024-11-09 18:42:07 -0500

    Add ModeWatcher component to layout for theme management

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,6 +2,7 @@
   import CartSheet from '$lib/components/cart-sheet.svelte';
   import Navbar from '$lib/components/shared/nav.svelte'
   import { setCartContextProvider } from '$lib/context/cart.svelte';
+  import { ModeWatcher } from "mode-watcher";
   import '../app.css'
 
 	let { children } = $props();
@@ -9,6 +10,7 @@
   setCartContextProvider()
 </script>
 
+<ModeWatcher />
 <Navbar />
 <main class="container mx-auto px-4 sm:px-6 lg:px-8 py-8">
   {@render children()}
