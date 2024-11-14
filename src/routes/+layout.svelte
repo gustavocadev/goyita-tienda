fe38c77938848068e4fadc940222bd5a4b44a055
gustavocadev <gustavocadev@gmail.com>
2024-11-13 19:31:58 -0500

    refactor: formatting code

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,19 +1,17 @@
 <script lang="ts">
   import CartSheet from '$lib/components/cart-sheet.svelte';
-  import Navbar from '$lib/components/shared/nav.svelte'
+  import Navbar from '$lib/components/shared/nav.svelte';
   import { setCartContextProvider } from '$lib/context/cart.svelte';
-  import { ModeWatcher } from "mode-watcher";
-  import '../app.css'
+  import { ModeWatcher } from 'mode-watcher';
+  import '../app.css';
 
-	let { children, data } = $props();
+  let { children, data } = $props();
 
-  setCartContextProvider()
+  setCartContextProvider();
 </script>
 
 <ModeWatcher />
-<Navbar
-  user={data.user}
-/>
+<Navbar user={data.user} />
 <main class="container mx-auto px-4 sm:px-6 lg:px-8 py-8">
   {@render children()}
 </main>
