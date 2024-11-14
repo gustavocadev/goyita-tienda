9a5f23d1a5f69859920f718a731e2b0c7c9b7c7c
gustavocadev <gustavocadev@gmail.com>
2024-11-13 19:31:07 -0500

    Create a user cart if cartItems is not empty

--- src/routes/login/+page.server.ts
+++ src/routes/login/+page.server.ts
@@ -1,6 +1,6 @@
 import { fail, redirect } from '@sveltejs/kit';
 import { zod } from 'sveltekit-superforms/adapters';
-import { superValidate } from 'sveltekit-superforms/server';
+import { message, superValidate } from 'sveltekit-superforms/server';
 import { z } from 'zod';
 
 export const load = async ({ locals }) => {
@@ -14,6 +14,13 @@ export const load = async ({ locals }) => {
 const schema = z.object({
   email: z.string().email(),
   password: z.string().min(8),
+  cartItems: z.string().transform((val) => {
+    const parsedValue = JSON.parse(val) as {
+      quantity: number;
+      productId: string;
+    }[];
+    return parsedValue;
+  }),
 });
 
 export const actions = {
@@ -23,11 +30,32 @@ export const actions = {
     if (!form.valid) {
       return fail(400, { form });
     }
-    const { email, password } = form.data;
+    const { email, password, cartItems } = form.data;
+
     await new Promise((resolve) => setTimeout(resolve, 1000));
+    try {
+      const authData = await locals.pb
+        .collection('users')
+        .authWithPassword(email, password);
 
-    await locals.pb.collection('users').authWithPassword(email, password);
+      if (cartItems.length !== 0) {
+        const userId = authData.record.id;
+        const createdCart = await locals.pb.collection('cart').create({
+          user_id: userId,
+        });
 
-    redirect(302, '/');
+        for (const cartItem of cartItems) {
+          await locals.pb.collection('cart_items').create({
+            product_id: cartItem.productId,
+            cart_id: createdCart.id,
+            quantity: cartItem.quantity,
+          });
+        }
+      }
+    } catch (error) {
+      console.log({ error });
+      return message(form, 'Invalid email or password');
+    }
+    redirect(303, '/');
   },
 };
