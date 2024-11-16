ccae551a5de7855958d0cb059a145a85a5c5ef4c
gustavocadev <gustavocadev@gmail.com>
2024-11-16 11:06:59 -0500

    feat: implement cart management for authenticated users

--- src/routes/login/+page.server.ts
+++ src/routes/login/+page.server.ts
@@ -2,6 +2,7 @@ import { fail, redirect } from '@sveltejs/kit';
 import { zod } from 'sveltekit-superforms/adapters';
 import { message, superValidate } from 'sveltekit-superforms/server';
 import { z } from 'zod';
+import type { TypedPocketBase } from '../../../pocketbase-types.js';
 
 export const load = async ({ locals }) => {
   const isValidSession = locals.pb.authStore.isValid;
@@ -32,24 +33,43 @@ export const actions = {
     }
     const { email, password, cartItems } = form.data;
 
+    console.log(cartItems);
+
     await new Promise((resolve) => setTimeout(resolve, 1000));
     try {
       const authData = await locals.pb
         .collection('users')
         .authWithPassword(email, password);
 
+      // verify if the user has a cart
+      const userId = authData.record.id;
+
+      const cart = await locals.pb.collection('cart').getFullList();
+      if (cart.length === 0) {
+        await createCartByUserId(locals.pb, userId);
+      }
+
+      // if my cart (from local storage) is not empty, create a cart for the user
       if (cartItems.length !== 0) {
-        const userId = authData.record.id;
-        const createdCart = await locals.pb.collection('cart').create({
-          user_id: userId,
-        });
+        const cart = await locals.pb.collection('cart').getFullList();
+        const [userCart] = cart;
+
+        // search if the product is already in the cart(from db)
+        const cartItemsFromDb = await locals.pb
+          .collection('cart_items')
+          .getFullList();
 
         for (const cartItem of cartItems) {
-          await locals.pb.collection('cart_items').create({
-            product_id: cartItem.productId,
-            cart_id: createdCart.id,
-            quantity: cartItem.quantity,
-          });
+          const existsProductInDb = cartItemsFromDb.some(
+            (cartItemFromDb) => cartItemFromDb.product_id === cartItem.productId
+          );
+          if (!existsProductInDb) {
+            await locals.pb.collection('cart_items').create({
+              product_id: cartItem.productId,
+              cart_id: userCart.id,
+              quantity: cartItem.quantity,
+            });
+          }
         }
       }
     } catch (error) {
@@ -58,4 +78,10 @@ export const actions = {
     }
     redirect(303, '/');
   },
+};
+
+const createCartByUserId = (pb: TypedPocketBase, userId: string) => {
+  return pb.collection('cart').create({
+    user_id: userId,
+  });
 };
