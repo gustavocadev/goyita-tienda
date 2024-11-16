9fcf57a997b527ba1cc143845827caf35004bbba
gustavocadev <gustavocadev@gmail.com>
2024-11-16 09:17:12 -0500

    feat: add cart item card component for displaying product details

---
+++ src/lib/components/cart-item-card.svelte
@@ -0,0 +1,30 @@
+<script lang="ts">
+  import { Card, CardContent } from './ui/card';
+
+  type Props = {
+    productTitle: string;
+    productPrice: number;
+    productImg?: string | undefined;
+  };
+
+  let { productTitle, productPrice, productImg }: Props = $props();
+
+  // let
+</script>
+
+<div class="flex items-center gap-4">
+  <img
+    src={productImg}
+    alt="Vue.js Course Thumbnail"
+    width={120}
+    height={68}
+    class="rounded-lg object-cover"
+  />
+  <div class="flex-1">
+    <h3 class="font-medium">{productTitle}</h3>
+    <button class="text-red-500 text-sm mt-1">Eliminar</button>
+  </div>
+  <div class="text-right">
+    <p class="font-medium">S/{productPrice} SOL</p>
+  </div>
+</div>
