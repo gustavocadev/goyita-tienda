3a30588426d8e3c4116ed198a7c452a72de85bea
gustavocadev <gustavocadev@gmail.com>
2024-11-30 18:41:12 -0500

    feat: integrate ProductCounter component into cart sheet for quantity management

--- src/lib/components/cart-sheet.svelte
+++ src/lib/components/cart-sheet.svelte
@@ -1,38 +1,20 @@
 <script lang="ts">
-  import { Minus, Plus, Trash, WalletMinimal } from 'lucide-svelte';
+  import { WalletMinimal } from 'lucide-svelte';
   import { Button } from './ui/button';
   import { Sheet, SheetContent, SheetHeader, SheetTitle } from './ui/sheet';
   import { getCartContext } from '$lib/context/cart.svelte';
-  import { goto, invalidate } from '$app/navigation';
-  import { applyAction, enhance } from '$app/forms';
-  import type { SubmitFunction } from '@sveltejs/kit';
+  import { goto } from '$app/navigation';
+  import ProductCounter from './product-counter.svelte';
 
   let isLoading = $state(false);
 
   let {
-    decrementQuantity,
-    incrementQuantity,
     setOpenCartSheet,
     isOpenCartSheet,
     cartItems,
     toggleCartSheet,
-    isUserLoggedIn,
     getTotalAmount,
-    removeCartItem,
   } = getCartContext();
-
-  const handleIncrementSubmit: SubmitFunction = () => {
-    return async ({ result }) => {
-      applyAction(result);
-    };
-  };
-
-  const handleDecrementSubmit: SubmitFunction = () => {
-    return async ({ result }) => {
-      await applyAction(result);
-      invalidate('remove:items');
-    };
-  };
 </script>
 
 <Sheet
@@ -72,69 +54,7 @@
               </div>
               <div class="flex items-center justify-between">
                 <span class="font-medium">S/. {cartProduct.price}</span>
-                <div class="flex items-center gap-3 bg-white/5 rounded-lg">
-                  <form
-                    method="post"
-                    use:enhance={handleDecrementSubmit}
-                    action={isUserLoggedIn.value
-                      ? '/?/decrementItem'
-                      : undefined}
-                  >
-                    <input
-                      type="hidden"
-                      name="productId"
-                      value={cartProduct.productId}
-                    />
-                    <Button
-                      variant="ghost"
-                      size="icon"
-                      class="h-8 w-8"
-                      onclick={() => {
-                        if (
-                          !isUserLoggedIn.value &&
-                          cartProduct.quantity === 1
-                        ) {
-                          removeCartItem(cartProduct.productId);
-                          return;
-                        }
-
-                        decrementQuantity(cartProduct.productId);
-                      }}
-                      type={isUserLoggedIn.value ? 'submit' : 'button'}
-                    >
-                      {#if cartProduct.quantity === 1}
-                        <Trash class="size-3" />
-                      {:else}
-                        <Minus class="size-3" />
-                      {/if}
-                    </Button>
-                  </form>
-                  <span class="w-4 text-center">{cartProduct.quantity}</span>
-                  <form
-                    action={isUserLoggedIn.value
-                      ? '/?/incrementItem'
-                      : undefined}
-                    method="post"
-                    use:enhance={handleIncrementSubmit}
-                  >
-                    <input
-                      type="hidden"
-                      name="productId"
-                      value={cartProduct.productId}
-                    />
-                    <Button
-                      variant="ghost"
-                      size="icon"
-                      class="h-8 w-8"
-                      type={isUserLoggedIn.value ? 'submit' : 'button'}
-                      onclick={() => {
-                        incrementQuantity(cartProduct.productId);
-                      }}
-                    >
-                      <Plus class="size-3" />
-                    </Button>
-                  </form>
-                </div>
+                <ProductCounter {cartProduct} />
               </div>
             </div>
           </div>
