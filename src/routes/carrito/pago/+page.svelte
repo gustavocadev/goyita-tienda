a86e183f1b7e019dc382c221f9d279acf9abacff
gustavocadev <gustavocadev@gmail.com>
2024-11-30 10:46:48 -0500

    feat: add payment selection component to checkout page

---
+++ src/routes/carrito/pago/+page.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+  import CheckoutPayment from '$lib/components/checkout-payment.svelte';
+  import Separator from '$lib/components/ui/separator/separator.svelte';
+</script>
+
+<div class="space-y-2">
+  <h2><span class="font-bold">Paso 1:</span> Elige el metodo de pago</h2>
+  <Separator />
+  <CheckoutPayment />
+  <Separator />
+</div>
