1a7dfda5b7a4e086dfa5614c9c261323d47916c8
gustavocadev <gustavocadev@gmail.com>
2024-12-01 17:19:59 -0500

    feat: load order details with expanded order items and product information

--- src/routes/carrito/+layout.server.ts
+++ src/routes/carrito/+layout.server.ts
@@ -1,9 +1,28 @@
 import { redirect } from '@sveltejs/kit';
+import type {
+  OrderItemsResponse,
+  OrdersResponse,
+  ProductsResponse,
+} from '../../../pocketbase-types.js';
 
 export const load = async ({ locals }) => {
+  const order = await locals.pb.collection('orders').getFirstListItem<
+    OrdersResponse<{
+      order_items_via_order_id: OrderItemsResponse<{
+        product_id: ProductsResponse;
+      }>[];
+    }>
+  >('', {
+    sort: '-created',
+    // orders, products, product_prices
+    expand: 'order_items_via_order_id, order_items_via_order_id.product_id',
+  });
+
   if (!locals.user) {
     redirect(303, '/login');
   }
 
-  return {};
+  return {
+    order,
+  };
 };
