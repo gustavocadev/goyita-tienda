b1bf63231f4110e716d0af6bf34e7b3677c40f0b
gustavocadev <gustavocadev@gmail.com>
2024-12-01 17:19:43 -0500

    feat: update order retrieval to limit results and check total items before redirecting

--- src/routes/carrito/pago/+page.server.ts
+++ src/routes/carrito/pago/+page.server.ts
@@ -1,8 +1,11 @@
 import { redirect } from '@sveltejs/kit';
 
 export const load = async ({ locals }) => {
-  const orders = await locals.pb.collection('orders').getFullList();
-  if (orders.length === 0) {
+  const orders = await locals.pb.collection('orders').getList(1, 2, {
+    sort: '-created',
+  });
+
+  if (orders.totalItems === 0) {
     redirect(303, '/carrito');
   }
 
