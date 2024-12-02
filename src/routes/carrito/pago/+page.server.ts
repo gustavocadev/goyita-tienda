1a01e0bded4697cdcd0013e608db88c63c5dbead
gustavocadev <gustavocadev@gmail.com>
2024-12-01 19:57:35 -0500

    feat: implement order item increment and decrement actions with validation

--- src/routes/carrito/pago/+page.server.ts
+++ src/routes/carrito/pago/+page.server.ts
@@ -1,4 +1,8 @@
-import { redirect } from '@sveltejs/kit';
+import { fail, redirect, type Actions } from '@sveltejs/kit';
+import { valibot } from 'sveltekit-superforms/adapters';
+import { superValidate } from 'sveltekit-superforms/server';
+import * as v from 'valibot';
+import type { OrderItemsResponse } from '../../../../pocketbase-types.js';
 
 export const load = async ({ locals }) => {
   const orders = await locals.pb.collection('orders').getList(1, 2, {
@@ -9,5 +13,75 @@ export const load = async ({ locals }) => {
     redirect(303, '/carrito');
   }
 
-  return {};
+  return {
+    order: orders.items[0],
+  };
+};
+
+const schema = v.object({
+  orderItemId: v.pipe(v.string(), v.trim(), v.nonEmpty()),
+});
+
+export const actions: Actions = {
+  incrementOrderItem: async ({ locals, request }) => {
+    const form = await superValidate(request, valibot(schema));
+
+    if (!form.valid) {
+      return fail(500, { form });
+    }
+
+    const { data } = form;
+
+    const orderItem = await locals.pb
+      .collection('order_items')
+      .getOne<OrderItemsResponse>(data.orderItemId);
+
+    // await locals.pb.collection('order_items').update(data.orderItemId, {
+    //   quantity: orderItem.quantity + 1,
+    // });
+
+    // a reducer function to calculate the total amount
+
+    // await locals.pb.collection('orders').update('', {
+    //   total_amount: 0, // TODO: calculate total amount
+    // });
+    // console.log('incrementOrderItem');
+
+    return {};
+  },
+  decrementOrderItem: async ({ locals, request }) => {
+    const form = await superValidate(request, valibot(schema));
+
+    if (!form.valid) {
+      return fail(500, { form });
+    }
+
+    const { data } = form;
+
+    const orderItem = await locals.pb
+      .collection('order_items')
+      .getOne<OrderItemsResponse>(data.orderItemId);
+
+    // await locals.pb.collection('order_items').update(data.orderItemId, {
+    //   quantity: orderItem.quantity - 1,
+    // });
+
+    // a reducer function to calculate the total amount
+    const orderItems = await locals.pb.collection('order_items').getList(1, 1, {
+      // products, product_prices
+      expand: 'product_id, product_prices',
+    });
+
+    // console.log(orderItems.items[0].expand.product_id);
+    // const totalAmount = orderItems.reduce(
+    //   (acc, item) => acc + item.quantity * item.price,
+    //   0
+    // );
+
+    // await locals.pb.collection('orders').update('', {
+    //   total_amount: 0, // TODO: calculate total amount
+    // });
+
+    return {};
+  },
 };
