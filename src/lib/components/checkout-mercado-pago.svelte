ab5d3f0c4553a54b90e1272e9785634c14da81fc
gustavocadev <gustavocadev@gmail.com>
2024-12-07 16:40:10 -0500

    feat: add checkout form for Mercado Pago payment

---
+++ src/lib/components/checkout-mercado-pago.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+  import { enhance } from '$app/forms';
+  import Button from './ui/button/button.svelte';
+</script>
+
+<form action="?/createOrder" method="post" use:enhance>
+  <Button class="w-full" type="submit">Pagar con Mercado Pago</Button>
+</form>
