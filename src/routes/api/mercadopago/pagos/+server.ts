9d8da5d6776f2fa27b177af505107d10d51f3685
gustavocadev <gustavocadev@gmail.com>
2024-12-07 16:39:34 -0500

    Listen mercado pago webhook

---
+++ src/routes/api/mercadopago/pagos/+server.ts
@@ -0,0 +1,35 @@
+import { client } from '$lib/mercadopago';
+import type { RequestHandler } from '@sveltejs/kit';
+import { Payment } from 'mercadopago';
+
+type MpBody = {
+  data: {
+    id: string;
+  };
+};
+
+export const POST: RequestHandler = async ({ request, locals }) => {
+  const body: MpBody = await request.json();
+  console.log({ body });
+
+  // manage the payment in our system
+  const payment = await new Payment(client).get({ id: body.data.id });
+
+  if (payment.status === 'approved') {
+    console.log('approved');
+    // update the order status
+    // send an email
+    // locals.pb.collection('orders').update(payment.external_reference, {
+    //   status: 2,
+    // })
+    // if (status === 2){
+    //   throw new Error('El pago ya fue aprobado');
+    // }
+
+    // locals.pb.collection('invoices').create({})
+  }
+
+  return new Response(null, {
+    status: 200,
+  });
+};
