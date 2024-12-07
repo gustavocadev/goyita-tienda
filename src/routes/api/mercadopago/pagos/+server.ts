da0e3c659bcf0f08d9607ecfaf48772fba4d3e5b
gustavocadev <gustavocadev@gmail.com>
2024-12-07 18:34:59 -0500

    style: add comments for order status update and payment validation in payment handler

--- src/routes/api/mercadopago/pagos/+server.ts
+++ src/routes/api/mercadopago/pagos/+server.ts
@@ -19,13 +19,18 @@ export const POST: RequestHandler = async ({ request, locals }) => {
     console.log('approved');
     // update the order status
     // send an email
+
+    // update the order status
     // locals.pb.collection('orders').update(payment.external_reference, {
     //   status: 2,
     // })
+
+    // validate the payment status
     // if (status === 2){
     //   throw new Error('El pago ya fue aprobado');
     // }
 
+    // create an invoice
     // locals.pb.collection('invoices').create({})
   }
 
