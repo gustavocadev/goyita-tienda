9906d0d801a15f6b5b46ee55f7dbb535744c4418
gustavocadev <gustavocadev@gmail.com>
2024-12-07 19:21:51 -0500

    feat: update order loading to retrieve the last order item for the cart layout

--- src/routes/carrito/+layout.server.ts
+++ src/routes/carrito/+layout.server.ts
@@ -6,23 +6,24 @@ import type {
 } from '../../../pocketbase-types.js';
 
 export const load = async ({ locals }) => {
-  const order = await locals.pb.collection('orders').getFirstListItem<
+  const order = await locals.pb.collection('orders').getList<
     OrdersResponse<{
       order_items_via_order_id: OrderItemsResponse<{
         product_id: ProductsResponse;
       }>[];
     }>
-  >('', {
+  >(0, 1, {
     sort: '-created',
     // orders, products, product_prices
     expand: 'order_items_via_order_id, order_items_via_order_id.product_id',
   });
-
   if (!locals.user) {
     redirect(303, '/login');
   }
 
+  const [lastOrder] = order.items;
+
   return {
-    order,
+    order: lastOrder,
   };
 };
