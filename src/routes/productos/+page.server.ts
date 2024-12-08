07ae207aadf932859281d43fb3ce8cf02197ab3f
gustavocadev <gustavocadev@gmail.com>
2024-12-08 11:55:01 -0500

    feat: Implement server-side loading for product listing with filtering

---
+++ src/routes/productos/+page.server.ts
@@ -0,0 +1,23 @@
+import type {
+  ProductPricesResponse,
+  ProductsResponse,
+} from '../../../pocketbase-types.js';
+
+export const load = async ({ locals, request, depends }) => {
+  depends('filter:products');
+  const url = new URL(request.url);
+  const productName = url.searchParams.get('p');
+
+  const products = locals.pb.collection('products').getList<
+    ProductsResponse<{
+      product_prices_via_product_id?: ProductPricesResponse[];
+    }>
+  >(1, 9, {
+    expand: 'product_prices_via_product_id',
+    filter: productName ? `name ~ "${productName}"` : '',
+  });
+
+  return {
+    products,
+  };
+};
