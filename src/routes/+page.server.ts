4db86f7e3f69f823015bc629dfd8481d2296ca9b
gustavocadev <gustavocadev@gmail.com>
2024-12-08 11:55:14 -0500

    refactor: Simplify product loading logic by removing unnecessary parameters and filtering

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -5,18 +5,13 @@ import { message, superValidate } from 'sveltekit-superforms/server';
 import { valibot } from 'sveltekit-superforms/adapters';
 import * as v from 'valibot';
 
-export const load = async ({ locals, request, depends }) => {
-  depends('filter:products');
-  const url = new URL(request.url);
-  const productName = url.searchParams.get('p');
-
-  const products = locals.pb.collection('products').getFullList<
+export const load = async ({ locals }) => {
+  const products = locals.pb.collection('products').getList<
     ProductsResponse<{
       product_prices_via_product_id?: ProductPricesResponse[];
     }>
-  >({
+  >(1, 4, {
     expand: 'product_prices_via_product_id',
-    filter: productName ? `name ~ "${productName}"` : '',
   });
 
   return {
