bd6b9cb88b908b8d50805e13cb8b4021b07e7589
gustavocadev <gustavocadev@gmail.com>
2024-12-07 21:12:49 -0500

    feat: display empty cart component when cart is empty

--- src/routes/carrito/+page.svelte
+++ src/routes/carrito/+page.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
   import { enhance } from '$app/forms';
+  import EmptyCart from '$lib/components/empty-cart.svelte';
   import { Button } from '$lib/components/ui/button';
   import { Card, CardContent } from '$lib/components/ui/card';
   import { getCartContext } from '$lib/context/cart.svelte';
@@ -8,21 +9,25 @@
   let { getTotalAmount, cartItems } = getCartContext();
 </script>
 
-<Card class="row-span-1">
-  <CardContent
-    class="p-6 space-y-4 flex justify-center items-center flex-col h-full"
-  >
-    <div class="flex items-center gap-2 text-sm text-muted-foreground">
-      <CircleIcon class="size-4" />
-      <span>{cartItems.value.length} Productos</span>
-    </div>
-    <div class="flex justify-between items-center">
-      <span class="text-4xl font-bold">S/{getTotalAmount()}</span>
-    </div>
-    <form action="?/createOrder" method="post" use:enhance>
-      <Button class="w-full" size="lg" type="submit"
-        >Voy a comprar para mí 🛒</Button
-      >
-    </form>
-  </CardContent>
-</Card>
+{#if cartItems.value.length > 0}
+  <Card class="row-span-1">
+    <CardContent
+      class="p-6 space-y-4 flex justify-center items-center flex-col h-full"
+    >
+      <div class="flex items-center gap-2 text-sm text-muted-foreground">
+        <CircleIcon class="size-4" />
+        <span>{cartItems.value.length} Productos</span>
+      </div>
+      <div class="flex justify-between items-center">
+        <span class="text-4xl font-bold">S/{getTotalAmount()}</span>
+      </div>
+      <form action="?/createOrder" method="post" use:enhance>
+        <Button class="w-full" size="lg" type="submit"
+          >Voy a comprar para mí 🛒</Button
+        >
+      </form>
+    </CardContent>
+  </Card>
+{:else}
+  <EmptyCart />
+{/if}
