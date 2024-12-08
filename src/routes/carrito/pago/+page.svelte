c3bbab81e77d5dfcc26afc3036251826b0cc58ab
gustavocadev <gustavocadev@gmail.com>
2024-12-07 21:11:54 -0500

    feat: display empty cart component when no order data is available

--- src/routes/carrito/pago/+page.svelte
+++ src/routes/carrito/pago/+page.svelte
@@ -5,6 +5,7 @@
   import Separator from '$lib/components/ui/separator/separator.svelte';
   import Label from '$lib/components/ui/label/label.svelte';
   import Button from '$lib/components/ui/button/button.svelte';
+  import EmptyCart from '$lib/components/empty-cart.svelte';
 
   let { data } = $props();
   let selectedValue = $state('');
@@ -15,28 +16,35 @@
   ]);
 </script>
 
-<div class="space-y-2">
-  <h2><span class="font-bold">Paso 1:</span> Elige el metodo de pago</h2>
-  <RadioGroup.Root value="">
-    {#each paymentOptions as paymentOption}
-      <Button
-        onclick={() => {
-          selectedValue = paymentOption.value;
-        }}
-        variant="outline"
-        class="flex items-center justify-start"
-      >
-        <RadioGroup.Item value={paymentOption.value} id={paymentOption.value} />
-        <Label for={paymentOption.value}>{paymentOption.label}</Label>
-      </Button>
-    {/each}
-  </RadioGroup.Root>
+{#if data.order}
+  <div class="space-y-2">
+    <h2><span class="font-bold">Paso 1:</span> Elige el metodo de pago</h2>
+    <RadioGroup.Root value="">
+      {#each paymentOptions as paymentOption}
+        <Button
+          onclick={() => {
+            selectedValue = paymentOption.value;
+          }}
+          variant="outline"
+          class="flex items-center justify-start"
+        >
+          <RadioGroup.Item
+            value={paymentOption.value}
+            id={paymentOption.value}
+          />
+          <Label for={paymentOption.value}>{paymentOption.label}</Label>
+        </Button>
+      {/each}
+    </RadioGroup.Root>
 
-  {#if selectedValue === 'paypal'}
-    <Separator />
-    <CheckoutPayment />
-  {:else if selectedValue === 'mercado-pago'}
-    <Separator />
-    <CheckoutMercadoPago />
-  {/if}
-</div>
+    {#if selectedValue === 'paypal'}
+      <Separator />
+      <CheckoutPayment />
+    {:else if selectedValue === 'mercado-pago'}
+      <Separator />
+      <CheckoutMercadoPago />
+    {/if}
+  </div>
+{:else}
+  <EmptyCart />
+{/if}
