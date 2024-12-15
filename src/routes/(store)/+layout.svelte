a422dd8919889e119a0938fec1123f27432a3801
gustavocadev <gustavocadev@gmail.com>
2024-12-15 18:28:46 -0500

    feat: Decrease vertical padding in store layout for improved spacing

--- src/routes/(store)/+layout.svelte
+++ src/routes/(store)/+layout.svelte
@@ -2,6 +2,6 @@
   let { children } = $props();
 </script>
 
-<main class="container mx-auto px-4 sm:px-6 lg:px-8 py-32">
+<main class="container mx-auto px-4 sm:px-6 lg:px-8 py-16">
   {@render children()}
 </main>
