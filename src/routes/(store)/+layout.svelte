921bed1b3bc5b0750f37b600699ef5bc82d13269
gustavocadev <gustavocadev@gmail.com>
2024-12-15 18:31:04 -0500

    feat: Adjust main layout padding based on page URL for improved spacing

--- src/routes/(store)/+layout.svelte
+++ src/routes/(store)/+layout.svelte
@@ -1,7 +1,11 @@
 <script lang="ts">
+  import { page } from '$app/stores';
+
   let { children } = $props();
 </script>
 
-<main class="container mx-auto px-4 sm:px-6 lg:px-8 py-16">
+<main
+  class={`container mx-auto px-4 sm:px-6 lg:px-8 ${$page.url.pathname !== '/' ? 'py-32' : 'py-16'}`}
+>
   {@render children()}
 </main>
