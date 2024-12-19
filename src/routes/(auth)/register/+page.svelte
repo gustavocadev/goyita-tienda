3f8b9015a0c0b174fa11d40936ac436488212494
gustavocadev <gustavocadev@gmail.com>
2024-12-19 09:52:09 -0500

    fix: Handle duplicate email registration error by checking status code in registration response

--- src/routes/(auth)/register/+page.svelte
+++ src/routes/(auth)/register/+page.svelte
@@ -20,15 +20,8 @@
       if (result.type === 'success') {
         toast.success('Cuenta creada exitosamente');
       }
-      if (
-        $errors.email &&
-        $errors.fullName &&
-        $errors.password &&
-        $errors.passwordConfirm
-      )
-        return;
 
-      if (result.type === 'failure') {
+      if (result.type === 'failure' && result.status === 409) {
         toast.error('El correo electrónico ya está registrado');
       }
     },
