db2b98403523d9024cdec90ec716a82bce455a86
gustavocadev <gustavocadev@gmail.com>
2024-12-18 19:50:55 -0500

    feat: Add payment confirmation page with invoice retrieval logic

---
+++ src/routes/(store)/carrito/pago/confirmacion/+page.server.ts
@@ -0,0 +1,21 @@
+import { redirect } from '@sveltejs/kit';
+import type { InvoicesResponse } from '../../../../../../pocketbase-types';
+
+export const load = async ({ url, locals }) => {
+  const paymentId = url.searchParams.get('payment_id');
+  if (!paymentId) {
+    redirect(303, '/carrito/pago');
+  }
+
+  const invoices = await locals.pb
+    .collection('invoices')
+    .getFullList<InvoicesResponse>({
+      filter: `invoice_number = ${paymentId}`,
+    });
+
+  if (invoices.length === 0) {
+    // throw new Error('No invoices found');
+  }
+
+  return { invoice: invoices[0] };
+};
