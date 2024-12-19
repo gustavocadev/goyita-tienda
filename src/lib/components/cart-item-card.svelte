1031a148925c90672f1fb7bc0824dbadeaed26d7
gustavocadev <gustavocadev@gmail.com>
2024-12-19 09:14:52 -0500

    feat: Enhance CartItemCard to display product images with loading state using getProductById

--- src/lib/components/cart-item-card.svelte
+++ src/lib/components/cart-item-card.svelte
@@ -1,6 +1,9 @@
 <script lang="ts">
   import type { ProductCart } from '$lib/context/cart.svelte';
+  import { getProductById } from '$lib/helpers/get-product-by-id';
+  import { pb } from '$lib/pocketbase';
   import ProductCounter from './product-counter.svelte';
+  import Skeleton from './ui/skeleton/skeleton.svelte';
 
   type Props = {
     cartProduct: ProductCart;
@@ -11,13 +14,27 @@
 </script>
 
 <div class="flex items-center gap-2">
-  <img
-    src={cartProduct.img[0]}
-    alt="Product Thumbnail"
-    width={120}
-    height={68}
-    class="rounded-lg object-cover"
-  />
+  {#await getProductById(cartProduct.productId)}
+    <Skeleton class="" />
+  {:then product}
+    <img
+      src={product.img.length > 0
+        ? pb.files.getURL(product, product.img[0])
+        : 'https://via.placeholder.com/68x68'}
+      alt="Product Thumbnail"
+      width={64}
+      height={64}
+      class="rounded-lg object-cover"
+    />
+  {:catch error}
+    <img
+      src="https://via.placeholder.com/68x68"
+      alt="Product Thumbnail"
+      width={68}
+      height={68}
+      class="rounded-lg object-cover"
+    />
+  {/await}
   <div class="flex-1 space-y-1">
     <h3 class="font-semibold text-base">{cartProduct.name}</h3>
     <ProductCounter {cartProduct} {orderItemId} />
