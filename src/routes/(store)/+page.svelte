c702b5624ce3b5670a56c6dd168d1d76355128a1
gustavocadev <gustavocadev@gmail.com>
2024-12-19 08:17:42 -0500

    refactor: Refactor product display to use ProductCard component for improved code organization

--- src/routes/(store)/+page.svelte
+++ src/routes/(store)/+page.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+  import ProductCard from '$lib/components/product-card.svelte';
   import { Skeleton } from '$lib/components/ui/skeleton';
   import { pb } from '$lib/pocketbase';
   import { MetaTags } from 'svelte-meta-tags';
@@ -24,39 +25,18 @@
     {/each}
   {:then products}
     {#each products.items as product}
-      <div class="group relative">
-        <figure
-          class="w-full min-h-80 aspect-w-1 aspect-h-1 rounded-md overflow-hidden group-hover:opacity-75 lg:h-80 lg:aspect-none"
-        >
-          <img
-            src={product.img.length > 0
+      <ProductCard
+        product={{
+          id: product.id,
+          name: product.name,
+          description: product.description,
+          img:
+            product.img.length > 0
               ? pb.files.getURL(product, product.img[0])
-              : 'https://placehold.co/500'}
-            alt={product.description}
-            class="w-full h-full object-center object-cover lg:w-full lg:h-full"
-          />
-        </figure>
-        <div class="mt-4 flex justify-between">
-          <div>
-            <h3 class="text-sm text-gray-700 dark:text-gray-300">
-              <a href={`/product/${product.id}`}>
-                <span aria-hidden="true" class="absolute inset-0"></span>
-                {product.name}
-              </a>
-            </h3>
-          </div>
-          <p
-            class="text-sm font-medium text-gray-900 dark:text-gray-200
-          "
-          >
-            {#if product.expand?.product_prices_via_product_id}
-              S/. {product.expand?.product_prices_via_product_id[0].price}
-            {:else}
-              No esta disponible
-            {/if}
-          </p>
-        </div>
-      </div>
+              : 'https://placehold.co/500',
+          price: product.expand?.product_prices_via_product_id?.[0].price,
+        }}
+      />
     {/each}
   {/await}
 </div>
